3a222656f4b04c686ebae2ccd5184b031b616523
Your Name <pirj@mail.ru>
2011-12-19 15:16:01 +0700

    quotes slideshow

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,4 +6,16 @@ $(function() {
       el.siblings('span.rating').text(data)
     })
   })
+
+  setTimeout("change_quote()", 10000)
 })
+
+function change_quote() {
+  var current = $(".header .sub .quotes p.active")
+  current.fadeOut("slow", function() {
+    var next = current.removeClass('active').next()
+    if(next.text() == "") next = $(".header .sub .quotes p").first()
+    next.addClass('active').fadeIn("slow")
+  })
+  setTimeout("change_quote()", 10000)
+}
