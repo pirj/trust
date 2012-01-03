2f164728e5def4fcad1b461ccc62d631cc4c8c65
Your Name <pirj@mail.ru>
2012-01-04 01:09:05 +0700

    cron fix

---
+++ bin/publish.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+#coding: utf-8
+PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"  unless defined?(PADRINO_ENV)
+PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
+
+# Load our dependencies
+require 'rubygems' unless defined?(Gem)
+require 'bundler/setup'
+Bundler.require(:default, PADRINO_ENV)
+
+Padrino.after_load do
+  DataMapper.finalize
+end
+
+Padrino.load!
+
+def publish message, ratings, login
+  # TODO: add vk publishing
+  return if ratings.length == 0
+  names = ratings.map do |r| r.person.name end
+  if names.length > 3 then
+    names = names[0..2].join(', ') + "и ещё #{names.length - 3} общественным деятелям"
+  else
+    names = names.join(', ')
+  end
+  post = "#{message} #{names} на Народном рейтинге"
+  link = "http://grajdanin.org/person/view/#{ratings.first.person_id}"
+  reply = HTTParty.post("https://graph.facebook.com/#{login.uid}/feed", :body => {:access_token => login.token, :message => post, :link => link})
+  result = MultiJson.decode reply.body
+  ratings.each do |r| r.published = true; r.save end unless result['error'].nil?
+  puts "error: #{result['error']}" if result['error'].nil?
+  result['error'].nil? ? ratings.length : 0
+end
+
+##
+# Find all unpublished Ratings, get user tokens, and publish to wall
+logins = Rating.all(:published.not => true).accounts.logins(:token.not => nil)
+
+published = 0
+logins.each do |login|
+  ratings = login.account.ratings(:published.not => true)
+  positive = ratings.reject {|r| !r.positive}
+  negative = ratings - positive
+  published = published + publish("Я выразил доверие", positive, login)
+  published = published + publish("Я выразил недоверие", negative, login)
+end
+
+puts "Published #{published} ratings"
