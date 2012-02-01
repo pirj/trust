b46559d7ad70ccb3e7d210763cdc7edcdb4c3373
Your Name <pirj@mail.ru>
2012-02-01 20:26:48 +0700

    permanent logging in fixed

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -89,7 +89,7 @@ function facebook_auth(response) {
     var uid = response.authResponse.userID;
     var token = response.authResponse.accessToken;
     $.get("/auth/facebook?token="+token+"&uid="+uid, function(data, status){
-      $('.loggingin').removeClass('loggingvk')
+      $('.loggingin').removeClass('loggingfb')
       if(status == 'success'){
         $('#logins .fb').append($('<span>'+data+'</span>'))
         $('body').addClass('loggedinfb')
